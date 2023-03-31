b72a9e3ae61677ec37565b3220c559414df70707
FahmiEqubal <88677272+FahmiEqubal@users.noreply.github.com>
2023-03-31 12:06:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BajajHealth
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yomntc)
---
+++ index.js
@@ -0,0 +1,30 @@
+// // Import stylesheets
+// import './style.css';
+
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// Build and host a REST API (Method: POST) that takes in an array and returns the following:
+// 1. Status
+// 2. User ID
+// 3. Email ID
+// 4. College Roll Number
+// 5. Array for even numbers
+// 6. Array for odd numbers
+// 7. Array for alphabets, converted to uppercase
+// Please refer to Annexure (A) for request/response samples
+
+const user = document.getElementById("ID").value="";
+const email = document.getElementById("Email").value="";
+const Roll = document.getElementById("Roll").value="";
+const Even = document.getElementById("Even").value="";
+const Odd = document.getElementById("Odd").value="";
+const uppercase = document.getElementById("uppercase").value="";
+const status = document.getElementById("status").innerText="is_success";
+document.write(status)
+document.write(user)
+document.write(email)
+document.write(Roll)
+document.write(even)
+document.write(odd)
+document.write(uppercase)
